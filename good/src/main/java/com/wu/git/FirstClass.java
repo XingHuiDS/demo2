4dac4df83cdb2df7733f4890b5175c37d03baaef
wuxinghui <260537614@qq.com>
2019-07-22 20:48:09 +0800

    33

---
+++ good/src/main/java/com/wu/git/FirstClass.java
@@ -0,0 +1,18 @@
+package com.wu.git;
+
+/**
+ * @author wuxinghui
+ * @date 2019/7/22-16:50
+ */
+public class FirstClass {
+  public static void main(String[] args) {
+    System.out.println("aaaaaaaaaaa");
+    System.out.println("aaaaaaaaaaa");
+    System.out.println("aaaaaaaaaaa");
+    System.out.println("aaaaaaaaaaa");
+    System.out.println("bbbbbbbbb");
+  }
+  public  void test(){
+    System.out.println("a");
+  }
+}
